cda53f953d263ef524c3fd1c894f69860dca5988
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-08-16 23:59:03 -0300

    Created a new app and the SignIn screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles'
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>LGPD GAP ANALYSIS</Text>
+    </View>
+  );
+}
